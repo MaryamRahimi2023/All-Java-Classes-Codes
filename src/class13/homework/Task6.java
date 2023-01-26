31a8732897dfd2bed23c5d409632814997cb908e
MaryamRahimi2023 <maryam.qambari2009@gmail.com>
2023-01-26 00:19:51 -0500

    updates

--- src/class13/homework/Task6.java
+++ src/class13/homework/Task6.java
@@ -1,18 +1,22 @@
 package class13.homework;
 
+import java.util.Scanner;
+
 public class Task6 {
 
     public static void main(String[] args) {
 
         // 6) How would you swap  2 strings without a temporary variable?
 
-        String str1="I am sleepy";
-        String str2="go sleep";
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please enter your name and last name: ");
+        String str1=scanner.next();
+        String str2=scanner.next();
 
         System.out.println("Before: " +str1+" "+str2);
 
         str1=str1+str2;
-        str2=str1.substring(0,str1.length()-str2.length());
+        str2=str1.substring(0,str2.length());
         str1=str1.substring(str2.length());
 
         System.out.println("After: "+str1+" "+str2);
