02a7ea28e18f42cc4a6bf6f1c00524dbb184c312
MaryamRahimi2023 <maryam.qambari2009@gmail.com>
2023-03-15 03:07:33 -0400

    updates

--- src/test.java
+++ src/test.java
@@ -29,42 +29,6 @@ public class test {
         }*/
 
 
-        /*
-        1) How would handle InputMismatchException? Input Mismatch Exception
-        when user enters mismatch value then programmer expected.
-         */
-       /* Scanner scan = new Scanner(System.in);
-        try {
-            System.out.println("Enter a number: ");
-            int n = scan.nextInt();
-        } catch (InputMismatchException ime) {
-            System.out.println("Input received must be a whole number");
-        }
-*/
-        /*Scanner scan = new Scanner(System.in);
-
-
-        try {
-            System.out.print("Enter your account number: ");
-            int accountNumber = scan.nextInt();
-        } catch (InputMismatchException invalidAC) {
-            System.out.println("Invalid input. Please enter numeric values for your account number");
-
-        }*/
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-
-        try {
-            String name = scan.nextLine();
-            System.out.println("Hello, " + name + "!");
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please enter a string.");
-        }
-
-        String str = "name"; // initializing a String variable inside the try block
-
-       System.out.println("The value of str is: " + str);
-
     }
 }
 
